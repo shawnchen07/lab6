5a3050adf63363b1ff70aaab8991691222d1b3bd
Shawn Chen <syc010@ucsd.edu>
2015-02-12 14:08:20 -0800

    Putting together lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,24 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/"+projectID);
+
+	var url = '/project/'+idNumber;
+	$.get(url, addProject);
+
+	// console.log("I am here!");
+}
+
+function addProject(result) {
+  console.log("In the projectHTML method...");
+
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
+
+  $("#project" + result['id']+" .details").html(projectHTML);
 }
 
 /*
@@ -35,4 +53,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addPalette);
+}
+
+function addPalette(result) {
+	var colors = result;
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
